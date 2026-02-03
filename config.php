e77317903296d867efb3bfba8fee848026045b4e
eroncero <eroncero@digitalstack.es>
2026-02-03 20:44:42 +0100

    General refactor

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = getenv('MOODLE_DB_TYPE');
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = getenv('MOODLE_DB_HOST');
+$CFG->dbname    = getenv('MOODLE_DB_NAME');
+$CFG->dbuser    = getenv('MOODLE_DB_USER');
+$CFG->dbpass    = getenv('MOODLE_DB_PASS');
+$CFG->prefix    = getenv('MOODLE_DB_PREFIX');
+
+$CFG->wwwroot   = getenv('MOODLE_URL');
+$CFG->dataroot  = getenv('MOODLE_DATAROOT');
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// // it is intentional because it prevents trailing whitespace problems!
